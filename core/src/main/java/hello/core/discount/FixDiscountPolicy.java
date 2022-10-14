d7c2bcce74bb9318b51ae0db8fd7871aa8c9d70a
ye1n <uyeinyein@gmail.com>
2022-10-14 14:36:39 +0900

    주문과 할인 도메인 개발

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountFixAmount = 1000;   // 1000원 할인
+
+    // 등급이 VIP인 회원은 1000원 할인 적용
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }else {
+            return 0;
+        }
+    }
+}
