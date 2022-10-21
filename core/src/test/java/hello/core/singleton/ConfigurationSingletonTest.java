99cbc3e61631f0fdd266424e8a1ae3f5de522e6d
ye1n <uyeinyein@gmail.com>
2022-10-21 14:47:10 +0900

    @Configuration과 바이트코드 조작의 마법

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.annotation.Annotation;
+
 public class ConfigurationSingletonTest {
 
     @Test
@@ -25,5 +27,13 @@ public class ConfigurationSingletonTest {
         System.out.println("orderService -> memberRepository = " + memberRepository2);
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+    }
+    
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean);
     }
 }
