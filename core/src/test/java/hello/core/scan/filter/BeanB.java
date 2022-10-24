7dc69256064d407fbc13a206ddbf80383f35fa52
ye1n <uyeinyein@gmail.com>
2022-10-24 15:34:16 +0900

    필터

---
+++ core/src/test/java/hello/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package hello.core.scan.filter;
+
+@MyExcluedComponent
+public class BeanB {
+}
---
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,40 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
+    }
+
+//    includeFilters : 컴포넌트 스캔 대상을 추가로 지정한다.
+//    excludeFilters : 컴포넌트 스캔에서 제외할 대상을 지정한다.
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcluedComponent.class)
+    )
+
+    static class ComponentFilterAppConfig {
+
+    }
+}
