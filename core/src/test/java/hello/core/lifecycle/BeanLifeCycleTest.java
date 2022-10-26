a39202772cdc6881ae3a30d69e7642ab4e7b89c0
ye1n <uyeinyein@gmail.com>
2022-10-26 11:31:00 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,7 +19,8 @@ public class BeanLifeCycleTest {
     static class LifeCycleConfig {
 
 //        설정 정보에 초기화 소멸 메서드 지정
-        @Bean(initMethod = "init", destroyMethod = "close")
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("https://hello-spring.dev");
